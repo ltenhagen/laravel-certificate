02744ee3b63ba425e1cb577118c1d01cd68b8122
Lorenzo ten Hagen <lorenzo.tenhagen@foursites.nl>
2022-03-25 10:41:35 +0100

    finished section 07

---
+++ resources/views/components/category-dropdown.blade.php
@@ -0,0 +1,18 @@
+<x-dropdown>
+    <x-slot name="trigger">
+        <button class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold lg:w-32 text-left flex lg:inline-flex">
+            {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+
+            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
+        </button>
+    </x-slot>
+
+    <x-dropdown-item href="{{ App\Providers\RouteServiceProvider::HOME }}" :active="request()->routeIs('home') && !request('category')">All</x-dropdown-item>
+
+    @foreach ($categories as $category)
+        <x-dropdown-item
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            :active="request('category') === $category->slug"
+        >{{ ucwords($category->name) }}</x-dropdown-item>
+    @endforeach
+</x-dropdown>
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -6,24 +6,7 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
-            <x-dropdown>
-                <x-slot name="trigger">
-                    <button class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold lg:w-32 text-left flex lg:inline-flex">
-                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
-
-                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
-                    </button>
-                </x-slot>
-
-                <x-dropdown-item href="{{ App\Providers\RouteServiceProvider::HOME }}" :active="request()->routeIs('home')">All</x-dropdown-item>
-
-                @foreach ($categories as $category)
-                    <x-dropdown-item
-                        href="{{ route('categories.show', $category) }}"
-                        :active='request()->is("categories/{$category->slug}")'
-                    >{{ ucwords($category->name) }}</x-dropdown-item>
-                @endforeach
-            </x-dropdown>
+            <x-category-dropdown />
         </div>
 
         <!-- Other Filters -->
@@ -50,12 +33,15 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
-                <input
-                    type="text" name="search"
-                    placeholder="Find something"
-                    class="bg-transparent placeholder-black font-semibold text-sm"
-                    value="{{ request('search') }}">
+            <form method="GET" action="/">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                <input type="text"
+                       name="search"
+                       placeholder="Find something"
+                       class="bg-transparent placeholder-black font-semibold text-sm"
+                       value="{{ request('search') }}">
             </form>
         </div>
     </div>
